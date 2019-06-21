3d9cbe2b6b2bcc1cd601ccab29f05991f433aabe
Jouda Hidri <hidrijouda@gmail.com>
2019-06-22 00:09:27 +0200

    Add validation script

---
+++ authion/validation.groovy
@@ -0,0 +1,11 @@
+def requireValidation (Closure timeout, Closure input) {
+        def date = new Date()
+        int dayOfTheWeek = date[Calendar.DAY_OF_WEEK]
+        def hour = date.format('HH', TimeZone.getTimeZone('Europe/Berlin'))
+
+        if (dayOfTheWeek == Calendar.FRIDAY || hour.toInteger() >= 16 ) {
+            timeout (time: 15, unit: 'SECONDS') {
+                input ('Validation is required')
+            }
+        }
+    }
